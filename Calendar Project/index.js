e56eeff304cf0df0eccb23ccb9d44e929a54204a
seshambhargavi12 <seshambhargavi12@gmail.com>
2026-04-06 19:06:38 +0530

    Add files via upload

---
+++ Calendar Project/index.js
@@ -0,0 +1,29 @@
+const monthNameE1=document.getElementById("month");
+const dayNameE1=document.getElementById("day");
+const dayNumE1=document.getElementById("day-number");
+const yearE1=document.getElementById("year")
+
+const date= new Date();
+const month=date.getMonth();
+monthNameE1.innerText=date.toLocaleString("en", {
+    month:"long"
+});
+
+dayNameE1.innerText=date.toLocaleString("en",{
+    weekday:"long"
+});
+
+dayNumE1.innerText=date.getDate();
+
+yearE1.innerText=date.getFullYear();
+
+const video = document.querySelector('.sunrise');
+
+video.addEventListener('mouseenter', () => {
+  video.play();
+});
+
+video.addEventListener('mouseleave', () => {
+  video.pause();
+  video.currentTime = 0; // Optional: resets video to beginning
+});
